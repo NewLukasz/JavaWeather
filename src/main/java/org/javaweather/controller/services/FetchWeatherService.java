6d16e168b456c5b5e51b5c1c84fe657f6e90558f
NewLukasz <Kilijanskilukasz@gmail.com>
2021-08-19 17:54:45 +0200

    Prepare files to create fetch weather services

---
+++ src/main/java/org/javaweather/controller/services/FetchWeatherService.java
@@ -0,0 +1,11 @@
+package org.javaweather.controller.services;
+
+public class FetchWeatherService {
+
+    private String city;
+    private String temporaryDateInString;
+
+    public FetchWeatherService(String temporaryDateInString, String city){
+
+    }
+}
