2c97f315beaf0942e4ee1a2418db00770c159eef
NewLukasz <Kilijanskilukasz@gmail.com>
2021-08-20 13:48:58 +0200

    fetch weather service are available to return data

--- src/main/java/org/javaweather/controller/services/FetchWeatherService.java
+++ src/main/java/org/javaweather/controller/services/FetchWeatherService.java
@@ -10,14 +10,29 @@ import org.json.JSONObject;
 
 
 import java.io.IOException;
+import java.util.List;
 
 public class FetchWeatherService {
 
     private String city;
     private String temporaryDateInString;
+    private JSONObject jsonWithWeatherData;
+    private Boolean getWeatherDataStatus;
 
     public FetchWeatherService(String temporaryDateInString, String city){
-        doHttpToGetForecastForFiveDays(city);
+        this.getWeatherDataStatus = false;
+        checkIfCityIsFoundAndAssignIfYes(city);
+    }
+
+    private boolean checkIfCityIsFoundAndAssignIfYes(String city){
+        JSONObject jsonWithResponseData = getApiResponse(city);
+        String messageFromApi = getMessageFromJsonApiResponse(jsonWithResponseData);
+        if(messageFromApi.equals("City is found")){
+            this.jsonWithWeatherData=jsonWithResponseData;
+            return this.getWeatherDataStatus=true;
+        }else{
+            return this.getWeatherDataStatus=false;
+        }
     }
 
     private String getMessageFromJsonApiResponse(JSONObject jsonObjectWithMessage){
@@ -25,48 +40,21 @@ public class FetchWeatherService {
         return MessageCodes.getMessageBasedCode(codeOfMessage);
     }
 
-    public JSONObject doHttpToGetForecastForFiveDays(String city){
-        city="Lubweqweqweqlin";
+    private JSONObject getApiResponse(String city){
         String urlToApi= "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+ApiData.getApiKey();
         CloseableHttpClient client = HttpClients.createDefault();
         HttpGet get = new HttpGet(urlToApi);
         CloseableHttpResponse response = null;
-        JSONObject jsonObject = null;
         try {
             response = client.execute(get);
             HttpEntity entity = response.getEntity();
-            String str = EntityUtils.toString(entity);
-            System.out.println(str);
-            jsonObject = new JSONObject(str);
-            getMessageFromJsonApiResponse(jsonObject);
+            JSONObject jsonWithResponseData = new JSONObject(EntityUtils.toString(entity));
             response.close();
+            return jsonWithResponseData;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return jsonObject;
+        return null;
     }
 
-    public static void doHttpGetWithOpenWeather(){
-
-        String cityName ="Lublin";
-
-        String url="https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=842bc442024d008401d44dfcb60ce43a";
-        //String url="https://api.openweathermap.org/data/2.5/forecast?id=756135&appid=842bc442024d008401d44dfcb60ce43a";
-
-
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet get = new HttpGet(url);
-        CloseableHttpResponse resp = null;
-        try {
-            resp = client.execute(get);
-            HttpEntity entity = resp.getEntity();
-            //System.out.println(EntityUtils.toString(entity));
-            String str = EntityUtils.toString(entity);
-            JSONObject jsonObject = new JSONObject(str);
-            System.out.println(jsonObject);
-            resp.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
